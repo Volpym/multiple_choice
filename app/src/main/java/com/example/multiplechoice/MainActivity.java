66868fb3222f3a280e86f3f04de17140488db44f
Vasilis Alepopoulos <vasilisalepopoulos@gmail.com>
2019-10-05 15:32:51 +0300

    Fixes #355. This is just a test for chris

--- app/src/main/java/com/example/multiplechoice/MainActivity.java
+++ app/src/main/java/com/example/multiplechoice/MainActivity.java
@@ -2,12 +2,13 @@ package com.example.multiplechoice;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.database.SQLException;
-import android.database.sqlite.SQLiteDatabase;
-import android.database.sqlite.SQLiteOpenHelper;
+import android.os.AsyncTask;
 import android.os.Bundle;
 
-import java.io.File;
+import org.json.JSONException;
+
+import java.io.IOException;
+import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -16,5 +17,41 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+
+
+
+       getQuestionFromDB.execute();
+
+
+    }
+
+
+    private  class getQuestionFromDB extends AsyncTask<Void, Void, ArrayList<Question>> {
+        @Override
+        protected ArrayList<Question> doInBackground(Void... voids) {
+            ArrayList<Question> questionnaire = new ArrayList<>();
+            Question q = new Question();
+            try {
+                for (int i = 0; i < q.setJson().length(); i++) {
+                    Question newQuestion = new Question();
+                    newQuestion.json = q.json;
+                    newQuestion.setQuestion_id(i);
+                    newQuestion.setQuestion_txt(i);
+                    questionnaire.add(newQuestion);
+                }
+            } catch (IOException | JSONException e) {
+                e.printStackTrace();
+            }
+
+            return null;
+        }
+        @Override
+        protected void onPostExecute(ArrayList<Question> questionnaire) {
+            super.onPostExecute(questionnaire);
+    }
     }
 }
+
+
+
+
--- app/src/main/java/com/example/multiplechoice/Question.java
+++ app/src/main/java/com/example/multiplechoice/Question.java
@@ -1,7 +1,72 @@
 package com.example.multiplechoice;
 
 
-public class Question {
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.URL;
 
 
+public class Question{
+     int question_id;
+     String question_txt ;
+     JSONArray json ;
+
+
+    public int setQuestion_id(int i) {
+        try {
+            this.question_id = this.json.getJSONObject(i).getInt("id");
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
+        return question_id;
+    }
+
+    public String setQuestion_txt(int i) {
+        try {
+            this.question_txt = this.json.getJSONObject(i).getString("Question");
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
+
+        return question_txt;
+    }
+
+    public JSONArray setJson()throws JSONException, IOException {
+        java.net.HttpURLConnection connection;
+        URL url = new URL("http://192.168.1.5/icd_tei_ser/script/user/get_questions.php");
+        connection = (java.net.HttpURLConnection) url.openConnection();
+        connection.setRequestMethod("GET");
+        connection.setRequestProperty("Content-length", "0");
+        connection.setUseCaches(false);
+        connection.setConnectTimeout(0);
+        connection.setReadTimeout(0);
+        connection.connect();
+        int status = connection.getResponseCode();
+        JSONArray json = null;
+
+
+        switch (status) {
+            case 200:
+            case 201:
+
+                BufferedReader br = new BufferedReader(new InputStreamReader(connection.getInputStream()));
+                StringBuilder sb = new StringBuilder();
+                String line;
+
+                while ((line = br.readLine()) != null) {
+                    sb.append(line + "\n");
+                }
+
+                this.json = new JSONArray(sb.toString());
+                br.close();
+        }
+        return this.json;
+
+    }
 }
+
+
